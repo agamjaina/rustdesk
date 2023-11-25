3dd83b98473e805e8bd3d00471d4ea7d5d966bfe
agamjaina <67259661+agamjaina@users.noreply.github.com>
2023-11-25 22:44:51 +0530

    Update tabbar_widget.dart

--- flutter/lib/desktop/widgets/tabbar_widget.dart
+++ flutter/lib/desktop/widgets/tabbar_widget.dart
@@ -406,7 +406,7 @@ class DesktopTab extends StatelessWidget {
                         Offstage(
                             offstage: !showTitle,
                             child: const Text(
-                              "RustDesk",
+                              "BestDesk",
                               style: TextStyle(fontSize: 13),
                             ).marginOnly(left: 2))
                       ]).marginOnly(
